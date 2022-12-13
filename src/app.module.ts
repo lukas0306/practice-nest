42f342862be5aac5c4ffd0999f9c8f9240669a83
lukas0306 <lanosh195@gmail.com>
2022-12-13 12:12:11 +0900

    [Practice] NestJS

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MoviesModule } from './movies/movies.module';
+
+
+@Module({
+  imports: [MoviesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+    constructor (private readonly movieService: MoviesService){
+
+    }
+
+    @Get()
+    getAll(){
+        return this.movieService.getAll();
+    }
+
+    @Get('search')
+    search(@Query('year') searchingYear:string){
+        return `We are searching for a moive made after: ${searchingYear} `
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieId:number){
+        return this.movieService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData:CreateMovieDto){
+        return this.movieService.create(movieData);
+    }
+
+    @Delete('/:id')
+    delete(@Param('id') movieId:number){
+        return this.movieService.deleteOne(movieId)
+    }
+    
+    @Patch('/:id')
+    patch(@Param('id') movieId:number, @Body() updateData:UpdateMovieDto){
+        return this.movieService.update(movieId,updateData)
+    }
+    // router '/'없어도 됨 ':id'
+
+    
+} 
